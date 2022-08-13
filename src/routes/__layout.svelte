3155fda0b991ebca47f53f1d691f37f6b544ae6f
EnriqueSanchezDev <enriquesanchezsanabria@gmial.com>
2022-08-13 05:50:45 +0100

    inital5

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -39,6 +39,7 @@
 
 </main>
 
+<!--
 <footer>
   <section id="footer" class="bg-light d-flex align-items-center">
     <div class="container-lg">
@@ -49,6 +50,40 @@
 
   </section>
 </footer>
+-->
+<div class="footer-basic bg-dark">
+  <footer>
+    <!--<p class="mb-4">_____________________________________________</p>-->
+    <div class="social">
+    
+      <a href="https://twitter.com/Orbisnauta_">
+        <i class="bi bi-twitter text-white">
+        </i>
+      </a>
+      <a href="https://www.linkedin.com/company/orbisnauta/">
+        <i class="bi bi-linkedin text-white ">
+        </i>
+      </a>
+      <a href="https://www.facebook.com/orbisnauta">
+        <i class="bi bi-facebook text-white ">
+        </i>
+      </a>
+      <a href="https://www.instagram.com/orbisnauta/">
+          <i class="bi bi-instagram text-white ">
+          </i>
+        </a>
+    </div>
+    <!--<ul class="list-inline">
+        <li class="list-inline-item"><a href="#">Home</a></li>
+        <li class="list-inline-item"><a href="#">Services</a></li>
+        <li class="list-inline-item"><a href="#">About</a></li>
+        <li class="list-inline-item"><a href="#">Terms</a></li>
+        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+    </ul>-->
+    <p class="copyright">Copyright Ⓒ 2022 Orbisnauta</p>
+  </footer>
+</div>
+
 
 <style>
 
@@ -75,6 +110,55 @@ img{
 
 li{
   list-style: none;
+}
+
+.footer-basic {
+  padding:20px 0;
+  background-color:#ffffff;
+  color:#4b4c4d;
+}
+
+
+
+.footer-basic ul a {
+  color:inherit;
+  text-decoration:none;
+  opacity:0.8;
+}
+
+.footer-basic ul a:hover {
+  opacity:1;
+}
+
+.footer-basic .social {
+  text-align:center;
+  padding-bottom:10px;
+}
+
+.footer-basic .social > a {
+  font-size: 30px;
+  width:40px;
+  height:40px;
+  line-height:40px;
+  display:inline-block;
+  text-align:center;
+  border-radius:50%;
+  /*border:1px solid #ccc;*/
+  margin:0 8px;
+  color:inherit;
+  opacity:0.75;
+}
+
+.footer-basic .social > a:hover {
+  opacity:0.9;
+}
+
+.footer-basic .copyright {
+  margin-top:15px;
+  text-align:center;
+  font-size:13px;
+  color:#aaa;
+  margin-bottom:0;
 }
 
 
